f5343f460b27a39d53e9f1a96a44e9312cf66324
Jay Ko <jay.ko.developer@gmail.com>
2020-03-15 14:57:13 -0500

    cleaned the code in serilizer and view in Board

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView, CreateView, DeleteView
-from django.views.generic.detail import DetailView
 from rest_framework import generics, serializers
 
 from .models import Board
@@ -21,31 +18,3 @@ class BoardList(generics.ListCreateAPIView):
 class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
-
-
-"""
-class BoardList(ListView):
-    model = Board
-    template_name_suffix = '_list'
-
-class BoardCreate(CreateView):
-    model = Board
-    field = ['title', 'body', 'created', 'image']
-    template_name_suffix = '_create'
-    success_url = '/'
-
-class BoardUpdate(UpdateView):
-    model = Board
-    field = ['title', 'body', 'created', 'image']
-    template_name_suffix = '_update'
-    success_url = '/'
-
-class BoardDelete(DeleteView):
-    model = Board
-    template_name_suffix = '_delete'
-    success_url = '/'
-
-class BoardDetail(DetailView):
-    model = Board
-    template_name_suffix = '_detail'
-"""
